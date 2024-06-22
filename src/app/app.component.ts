cc12cacc81df271d49d66130e7acdf34c9535869
Anatolii Stoliarenko <anatolii.stoliarenko@gmail.com>
2024-06-22 20:45:51 +0200

    finished user-input component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from "@angular/core";
+
+import { HeaderComponent } from "./header/header.component";
+import { UserInputComponent } from "./user-input/user-input.component";
+import { InvestementInput } from "./investment-input.model";
+import { InvestmentService } from "./investment.service";
+import { InvestmentResultComponent } from "./investment-result/investment-result.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  templateUrl: './app.component.html',
+    selector: "app-root",
+    standalone: true,
+    templateUrl: "./app.component.html",
+    imports: [HeaderComponent, UserInputComponent, InvestmentResultComponent]
 })
-export class AppComponent {}
+export class AppComponent {
+  investmentService = inject(InvestmentService);
+
+  calculateInvestmentResults(data: InvestementInput) {
+    console.log(this.investmentService.calculateInvestmentResults(data));
+  }
+}
---
+++ src/app/investment-input.model.ts
@@ -0,0 +1,6 @@
+export interface InvestementInput {
+  initialInvestment: number;
+  duration: number;
+  expectedReturn: number;
+  annualInvestment: number;
+}
